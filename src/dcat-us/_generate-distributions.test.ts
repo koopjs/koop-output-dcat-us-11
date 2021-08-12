613235b54439574a342c9d96c5b8d35162a1a6ea
Andrew Tate <drewctate@gmail.com>
2021-08-12 15:10:11 -0500

    Separate distribution logic

---
+++ src/dcat-us/_generate-distributions.test.ts
@@ -0,0 +1,191 @@
+import { _generateDistributions } from './_generate-distributions';
+
+describe('_generateDistributions', () => {
+  const serviceUrl = 'https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/rest/services/Tahoe_Things/FeatureServer/0';
+  const getLandingPage = (id: string) => `https://my-site.hub.arcgis.com/datasets/${id}`;
+
+  it('add default distributions if dataset is a collection', () => {
+    const dataset = {
+      id: 'foo', // non-layer id
+      url: serviceUrl
+    };
+
+    const expected =  [{
+      '@type': 'dcat:Distribution',
+      title: 'ArcGIS Hub Dataset',
+      format: 'Web Page',
+      mediaType: 'text/html',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo'
+    },
+    {
+      '@type': 'dcat:Distribution',
+      title: 'ArcGIS GeoService',
+      format: 'ArcGIS GeoServices REST API',
+      mediaType: 'application/json',
+      accessURL: 'https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/rest/services/Tahoe_Things/FeatureServer/0'
+    },
+  ]
+
+    const distributions = _generateDistributions(dataset, getLandingPage(dataset.id));
+
+    expect(distributions.length).toBe(2)
+    expect(distributions).toEqual(expected);
+  })
+
+  it('adds default distributions if dataset is a layer', () => {
+    const dataset = {
+      id: 'foo_0', // layer id
+      url: serviceUrl
+    };
+
+    const expected =  [{
+      '@type': 'dcat:Distribution',
+      title: 'ArcGIS Hub Dataset',
+      format: 'Web Page',
+      mediaType: 'text/html',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo_0'
+    },
+    {
+      '@type': 'dcat:Distribution',
+      title: 'ArcGIS GeoService',
+      format: 'ArcGIS GeoServices REST API',
+      mediaType: 'application/json',
+      accessURL: 'https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/rest/services/Tahoe_Things/FeatureServer/0'
+    },
+    {
+      '@type': 'dcat:Distribution',
+      title: 'GeoJSON',
+      format: 'GeoJSON',
+      mediaType: 'application/vnd.geo+json',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo_0.geojson'
+    },
+    {
+      '@type': 'dcat:Distribution',
+      title: 'CSV',
+      format: 'CSV',
+      mediaType: 'text/csv',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo_0.csv'
+    }
+  ]
+
+    const distributions = _generateDistributions(dataset, getLandingPage(dataset.id));
+
+    expect(distributions.length).toBe(4)
+    expect(distributions).toEqual(expected);
+  });
+
+  it('adds KML and Shapefile if dataset is a layer AND has geometryType', () => {
+    const dataset = {
+      id: 'foo_0', // layer id
+      url: serviceUrl,
+      layer: {
+        geometryType: 'some thing'
+      }
+    };
+
+    const expected =  [{
+      '@type': 'dcat:Distribution',
+      title: 'KML',
+      format: 'KML',
+      mediaType: 'application/vnd.google-earth.kml+xml',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo_0.kml'
+    },
+    {
+      '@type': 'dcat:Distribution',
+      title: 'Shapefile',
+      format: 'ZIP',
+      mediaType: 'application/zip',
+      accessURL: 'https://my-site.hub.arcgis.com/datasets/foo_0.zip'
+    }
+  ]
+
+    const distributions = _generateDistributions(dataset, getLandingPage(dataset.id));
+
+    expect(distributions.length).toBe(6)
+    expect(distributions.slice(4)).toEqual(expected);
+  });
+
+  it('adds WFS and WMS if supported', () => {
+    const dataset = {
+      id: 'foo', // non-layer id
+      url: serviceUrl
+    };
+
+    const expectedWMSDistribution = {
+      '@type': 'dcat:Distribution',
+      title: 'OGC WMS',
+      format: 'OGC WMS',
+      mediaType: 'application/vnd.ogc.wms_xml',
+      accessURL: 'https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/services/Tahoe_Things/FeatureServer/WMSServer?request=GetCapabilities&service=WMS'
+    };
+
+    const expectedWFSDistribution = {
+      '@type': 'dcat:Distribution',
+      title: 'OGC WFS',
+      format: 'OGC WFS',
+      mediaType: 'application/vnd.ogc.wfs_xml',
+      accessURL: 'https://servicesqa.arcgis.com/Xj56SBi2udA78cC9/arcgis/services/Tahoe_Things/FeatureServer/WFSServer?request=GetCapabilities&service=WFS'
+    };
+
+    const justWFS = _generateDistributions({ ...dataset, supportedExtensions: 'WFSServer' }, getLandingPage(dataset.id));
+    expect(justWFS.length).toBe(3);
+    expect(justWFS.pop()).toEqual(expectedWFSDistribution);
+
+    const justWMS = _generateDistributions({ ...dataset, supportedExtensions: 'WMSServer' }, getLandingPage(dataset.id));
+    expect(justWMS.length).toBe(3);
+    expect(justWMS.pop()).toEqual(expectedWMSDistribution);
+
+    const allOGCServices = _generateDistributions({ ...dataset, supportedExtensions: 'WMSServer,WFSServer' }, getLandingPage(dataset.id));
+    expect(allOGCServices.length).toBe(4);
+    expect(allOGCServices.slice(2)).toEqual([expectedWFSDistribution, expectedWMSDistribution]);
+  });
+
+  it('adds custom distributions from metadata', () => {
+    const dataset = {
+      id: 'foo', // non-layer id
+      url: serviceUrl,
+      metadata: {
+        metadata: {
+          distInfo: {
+            distTranOps: {
+              onLineSrc: [
+                {
+                  orName: 'Title 1',
+                  protocol: 'TTL',
+                  linkage: 'https://some-url.com',
+                  orDesc: 'Fun Description'
+                },
+                {
+                  orName: 'Title 2',
+                  // no protocol
+                  linkage: 'https://some-url.com',
+                  // no description
+                }
+              ]
+            }
+          }
+        }
+      }
+    };
+
+    const distributions = _generateDistributions(dataset, getLandingPage(dataset.id));
+
+    expect(distributions.length).toBe(4);
+    expect(distributions.slice(2)).toEqual([
+      {
+        '@type': 'dcat:Distribution',
+        title: 'Title 1',
+        format: 'TTL',
+        accessURL: 'https://some-url.com',
+        description: 'Fun Description'
+      },
+      {
+        '@type': 'dcat:Distribution',
+        title: 'Title 2',
+        format: null,
+        accessURL: 'https://some-url.com',
+        description: null
+      }
+    ])
+  });
+});
--- src/dcat-us/dataset-formatter.ts
+++ src/dcat-us/dataset-formatter.ts
@@ -2,6 +2,7 @@ import * as _ from 'lodash';
 import { adlib, TransformsList } from 'adlib';
 import { isPage } from '@esri/hub-sites';
 import { baseTemplate } from './base-template';
+import { _generateDistributions } from './_generate-distributions';
 
 export function formatDcatDataset (dataset: any, siteUrl: string, dcatCustomizations: any = {}) {
   const landingPage = `${siteUrl}/datasets/${dataset.id}`;
@@ -37,7 +38,7 @@ export function formatDcatDataset (dataset: any, siteUrl: string, dcatCustomizat
     dcatDataset.keyword = ['ArcGIS Hub page'];
   }
 
-  dcatDataset.distribution = generateDistributions(dataset, landingPage);
+  dcatDataset.distribution = _generateDistributions(dataset, landingPage);
 
   // only add spatial property if there's an extent
   if (dataset.extent && dataset.extent.coordinates) {
@@ -78,173 +79,4 @@ function scrub (customizations: any) {
 export function dcatTemplate (customizations: any) {
   const customConfig = scrub(customizations || {});
   return Object.assign({}, baseTemplate, customConfig);
-}
-
-interface DistributionParameters {
-  landingPage: string;
-  serviceUrl: string;
-  downloadLinkFor: (type: string) => string;
-}
-
-/*
-* Generate DCAT Distributions
-*/
-function generateDistributions (dataset: any, landingPage: string) {
-  const distributionFns = [
-    getHubLandingPageDistribution,
-    getEsriGeoServiceDistribution
-  ];
-
-  if (isLayer(dataset)) {
-    distributionFns.push(getGeoJSONDistribution);
-    distributionFns.push(getCSVDistribution);
-
-    if (_.has(dataset, 'layer.geometryType')) {
-      distributionFns.push(getKMLDistribution);
-      distributionFns.push(getShapefileDistribution);
-    }
-  }
-
-  if (dataset.supportedExtensions?.includes('WFSServer')) {
-    distributionFns.push(getWFSDistribution);
-  }
-
-  if (dataset.supportedExtensions?.includes('WMSServer')) {
-    distributionFns.push(getWMSDistribution);
-  }
-
-  const params: DistributionParameters = {
-    landingPage,
-    serviceUrl: dataset.url,
-    downloadLinkFor: getDownloadLinkFn(landingPage, dataset)
-  };
-
-  const distributions = distributionFns.map(fn => fn(params));
-  const customDistributions = getCustomDistributions(dataset);
-
-  return [...distributions, ...customDistributions];
-}
-
-function isLayer (dataset: any) {
-  return /_/.test(dataset.id);
-}
-
-// HUBJS CANDIDATE
-function getDownloadLinkFn (landingPage: string, dataset: any) {
-  const spatialReference = _.get(dataset, 'server.spatialReference');
-
-  let queryStr = '';
-
-  if (spatialReference) {
-    const { latestWkid, wkid } = spatialReference;
-
-    if (wkid) {
-      const outSR = JSON.stringify({ latestWkid, wkid });
-      queryStr = `?outSR=${encodeURIComponent(outSR)}`;
-    }
-  }
-
-  return (ext: string) => `${landingPage}.${ext}${queryStr}`;
-}
-
-function getCustomDistributions (dataset: any) {
-  const distros = [];
-  const data = _.get(dataset, 'metadata.metadata.distInfo.distTranOps.onLineSrc');
-
-  if (Array.isArray(data)) {
-    for (const dist of data) {
-      distros.push({
-        '@type': 'dcat:Distribution',
-        title: dist.orName || null,
-        format: dist.protocol || null,
-        accessURL: dist.linkage || null,
-        description: dist.orDesc || null
-      });
-    }
-  }
-
-  return distros;
-}
-
-function ogcUrl (datasetUrl: string, type: 'WMS'|'WFS' = 'WMS') {
-  return datasetUrl.replace(/rest\/services/i, 'services').replace(/\d+$/, `${type}Server?request=GetCapabilities&service=${type}`);
-}
-
-function getHubLandingPageDistribution(params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'ArcGIS Hub Dataset',
-    format: 'Web Page',
-    mediaType: 'text/html',
-    accessURL: params.landingPage
-  };
-}
-
-function getEsriGeoServiceDistribution(params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'ArcGIS GeoService',
-    format: 'ArcGIS GeoServices REST API',
-    mediaType: 'application/json',
-    accessURL: params.serviceUrl
-  };
-}
-
-function getGeoJSONDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'GeoJSON',
-    format: 'GeoJSON',
-    mediaType: 'application/vnd.geo+json',
-    accessURL: params.downloadLinkFor('geojson')
-  };
-}
-function getCSVDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'CSV',
-    format: 'CSV',
-    mediaType: 'text/csv',
-    accessURL: params.downloadLinkFor('csv')
-  };
-}
-
-function getKMLDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'KML',
-    format: 'KML',
-    mediaType: 'application/vnd.google-earth.kml+xml',
-    accessURL: params.downloadLinkFor('kml')
-  };
-}
-
-function getShapefileDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'Shapefile',
-    format: 'ZIP',
-    mediaType: 'application/zip',
-    accessURL: params.downloadLinkFor('zip')
-  };
-}
-
-function getWMSDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'OGC WMS',
-    format: 'OGC WMS',
-    mediaType: 'application/vnd.ogc.wms_xml',
-    accessURL: ogcUrl(params.serviceUrl, 'WMS')
-  };
-}
-
-function getWFSDistribution (params: DistributionParameters) {
-  return {
-    '@type': 'dcat:Distribution',
-    title: 'OGC WMS',
-    format: 'OGC WMS',
-    mediaType: 'application/vnd.ogc.wms_xml',
-    accessURL: ogcUrl(params.serviceUrl, 'WMS')
-  };
 }
