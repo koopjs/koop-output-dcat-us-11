af5f8d9733d9c60b715647416a873ebbccdc3fb6
Caleb Pomar <cpsharksfan@gmail.com>
2021-09-14 14:10:00 -0600

    Add tests and code to support custom dcat configurations while generating the dcat-us 1.1 data stream

--- src/dcat-us/index.test.ts
+++ src/dcat-us/index.test.ts
@@ -7,8 +7,9 @@ import * as mockSiteModel from '../test-helpers/mock-site-model.json';
 function generateDcatFeed(
   siteItem,
   datasets,
+  dcatCustomizations?
 ) {
-  const dcatStream = getDataStreamDcatUs11(siteItem);
+  const dcatStream = getDataStreamDcatUs11(siteItem, dcatCustomizations);
 
   const docStream = readableFromArray(datasets); // no datasets since we're just checking the catalog
 
@@ -46,6 +47,88 @@ describe('generating DCAT-US 1.1 feed', () => {
     expect(chk1.modified).toBe('2021-07-27T20:25:19.723Z');
     expect(chk1.publisher).toEqual({ name: 'QA Premium Alpha Hub' });
     expect(chk1.contactPoint).toEqual({ '@type': 'vcard:Contact', fn: 'thervey_qa_pre_a_hub' });
+    expect(chk1.accessLevel).toBe('public');
+    expect(chk1.spatial).toBe('-121.118,38.7754,-119.009,39.359');
+    expect(chk1.theme).toEqual(['geospatial']);
+
+    expect(chk1.distribution).toBeInstanceOf(Array);
+    expect(chk1.distribution.length).toBe(6);
+  });
+
+  it('respects dcat customizations of overwritable attributes', async function () {
+    const feed = await generateDcatFeed(
+      mockSiteModel.item, 
+      [datasetFromApi],
+      {
+        description: '{{name}}', // overwrite existing attribute
+        customField: '{{name}}' // add new attribute
+      }
+    );
+
+    const chk1 = feed['dataset'][0];
+
+    expect(chk1['@type']).toBe('dcat:Dataset');
+    expect(chk1.identifier).toBe('https://download-test-qa-pre-a-hub.hubqa.arcgis.com/datasets/f4bcc1035b7d46cba95e977f4affb6be_0');
+    expect(chk1.license).toBe(null);
+    expect(chk1.landingPage).toBe('https://download-test-qa-pre-a-hub.hubqa.arcgis.com/datasets/f4bcc1035b7d46cba95e977f4affb6be_0');
+    expect(chk1.title).toBe('Tahoe places of interest');
+    expect(chk1.description).toBe('Tahoe places of interest');
+    expect(chk1.customField).toBe('Tahoe places of interest');
+    expect(chk1.keyword).toEqual([ 'Data collection', 'just modified' ]);
+    expect(chk1.issued).toBe('2021-01-29T15:34:38.000Z');
+    expect(chk1.modified).toBe('2021-07-27T20:25:19.723Z');
+    expect(chk1.publisher).toEqual({ name: 'QA Premium Alpha Hub' });
+    expect(chk1.contactPoint).toEqual({ '@type': 'vcard:Contact', fn: 'thervey_qa_pre_a_hub' });
+    expect(chk1.accessLevel).toBe('public');
+    expect(chk1.spatial).toBe('-121.118,38.7754,-119.009,39.359');
+    expect(chk1.theme).toEqual(['geospatial']);
+
+    expect(chk1.distribution).toBeInstanceOf(Array);
+    expect(chk1.distribution.length).toBe(6);
+  });
+
+  it('formats catalog correctly', async function () {
+    const feed = await generateDcatFeed(mockSiteModel.item, []);
+
+    expect(feed['@context']).toBe('https://project-open-data.cio.gov/v1.1/schema/catalog.jsonld');
+    expect(feed['@type']).toBe('dcat:Catalog');
+    expect(feed['conformsTo']).toBe('https://project-open-data.cio.gov/v1.1/schema');
+    expect(feed['describedBy']).toBe('https://project-open-data.cio.gov/v1.1/schema/catalog.json');
+    expect(Array.isArray(feed['dataset'])).toBeTruthy();
+    expect(feed['dataset'].length).toBe(0);
+  });
+
+  it('scrubs dcat customization of protected fields', async function () {
+    const feed = await generateDcatFeed(
+      mockSiteModel.item, 
+      [datasetFromApi],
+      {
+        '@type': '{{name}}',
+        identifier: '{{name}}',
+        landingPage: '{{name}}',
+        webService: '{{name}}',
+        distributions: '{{name}}',
+        contactPoint: {
+          '@type': '{{name}}',
+          fn: '{{owner}}',
+        }
+      }
+    );
+
+    const chk1 = feed['dataset'][0];
+
+    expect(chk1['@type']).toBe('dcat:Dataset');
+    expect(chk1.identifier).toBe('https://download-test-qa-pre-a-hub.hubqa.arcgis.com/datasets/f4bcc1035b7d46cba95e977f4affb6be_0');
+    expect(chk1.license).toBe(null);
+    expect(chk1.webService).toBe(undefined);
+    expect(chk1.landingPage).toBe('https://download-test-qa-pre-a-hub.hubqa.arcgis.com/datasets/f4bcc1035b7d46cba95e977f4affb6be_0');
+    expect(chk1.title).toBe('Tahoe places of interest');
+    expect(chk1.description).toBe('Description. Here be Tahoe things. You can do a lot here. Here are some more words. And a few more.<div><br /></div><div>with more words</div><div><br /></div><div>adding a few more to test how long it takes for our jobs to execute.</div><div><br /></div><div>Tom was here!</div>');
+    expect(chk1.keyword).toEqual([ 'Data collection', 'just modified' ]);
+    expect(chk1.issued).toBe('2021-01-29T15:34:38.000Z');
+    expect(chk1.modified).toBe('2021-07-27T20:25:19.723Z');
+    expect(chk1.publisher).toEqual({ name: 'QA Premium Alpha Hub' });
+    expect(chk1.contactPoint).toEqual({ fn: 'thervey_qa_pre_a_hub' });
     expect(chk1.accessLevel).toBe('public');
     expect(chk1.spatial).toBe('-121.118,38.7754,-119.009,39.359');
     expect(chk1.theme).toEqual(['geospatial']);
